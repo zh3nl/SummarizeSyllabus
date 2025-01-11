dc9d2a50f96724f2d6bf3dc9729002704d8acc3d
Sophia <sophiaray@ucsb.edu>
2025-01-11 11:26:22 -0800

    added more frontend intro

---
+++ summarizesyllabus/src/components/MainPage.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './MainPage.css';
+
+function MainPage(){
+    return (
+        <div>
+            <div className="header">Summarize Syllabus</div>
+            <section className='main-container'>
+                <div className='main-intro'>Scan. Simplify. Succeed.</div>
+            </section>
+        </div>
+    )
+
+}
+
+export default MainPage;

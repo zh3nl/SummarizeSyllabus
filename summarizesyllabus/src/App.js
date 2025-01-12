318e18f5c501628349f7b1dc588f155e90b5d98a
Sophia <sophiaray@ucsb.edu>
2025-01-11 17:59:09 -0800

    rid of mainpage

--- summarizesyllabus/src/App.js
+++ summarizesyllabus/src/App.js
@@ -1,11 +1,9 @@
 import './App.css';
 import FileUploader from './components/FileUploader';
-import MainPage from './components/MainPage';
 
 function App() {
   return (
     <div>
-      <MainPage />
       <FileUploader />
     </div>
   );
